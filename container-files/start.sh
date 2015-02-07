125d4dfde5b743b442ad453a59d6ea147d79abce
Przemyslaw Ozgo <linux@ozgo.info>
2015-02-07 17:00:40 +0000

    Base image changed to offcial CentOS7
    - supervisor

--- container-files/start.sh
+++ container-files/start.sh
@@ -14,3 +14,5 @@ if [ ! -f /etc/logstash/ssl/logstash-forwarder.crt ] ; then
 	cd /etc/logstash/ssl
     openssl req -x509 -batch -nodes -days 3650 -newkey rsa:2048 -keyout logstash-forwarder.key -out logstash-forwarder.crt
 fi
+# Start logstash
+/opt/logstash/bin/logstash agent -f /etc/logstash/logstash.conf
